459099a26450561917645f45da05c5eba4db1b68
Mikki <mblackwell@georgiacyber.org>
2022-01-12 18:26:10 -0500

    rearranged files and added beginning code to script file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +11,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  console.log("Hey! You clicked the button!");
+
+
+
+  return "Generated password will go here!";
+}
+
