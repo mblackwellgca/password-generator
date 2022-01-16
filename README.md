48244efa8f906fb484eeb4cde216f1241bb71112
Mikki <mblackwell@georgiacyber.org>
2022-01-15 20:58:38 -0500

    updated js file with initial code to run a random password generator task. Updated README with description, image of website demo, image of gif showing working code.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # password-generator
 Modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code.
+
+## Live Links
+
+
+## Description
+To randomly generate a password that meets certain criteria to create strong passwords on demand, for greater security.
+
+## Usage
+1. User will click the button "generate a password"
+2. After clicking the button, user will be prompted with questions to create a random secure password.
+
+![password generator](assets\images\password_generator.png)
+
+## Criteria 
+GIVEN I need a new, secure password
+* WHEN I click the button to generate a password
+    * THEN I am presented with a series of prompts for password criteria
+* WHEN prompted for password criteria
+    * THEN I select which criteria to include in the password
+* WHEN prompted for the length of the password
+    * THEN I choose a length of at least 8 characters and no more than 128 characters
+* WHEN asked for character types to include in the password
+    * THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+* WHEN I answer each prompt
+    * THEN my input should be validated and at least one character type should be selected
+* WHEN all prompts are answered
+    * THEN a password is generated that matches the selected criteria
+* WHEN the password is generated
+    * THEN the password is either displayed in an alert or written to the page
+
+3. Upon answering the prompts the password will generate and appear in the box. 
+
+![password generated](assets\images\password-generator.gif)
+
+## Credits:
+* Sandra Smith
+* Christopher Ponzio https://github.com/ChristopherPonzio
+
+## License:
+MIT License
--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 function generatePassword() {
-  console.log("Hey! You clicked the button!");
+  var secure = [""];
+  var choose = [""];
+  var length = window.prompt("How many characters(between 8 and 128)?");
 
+// Created Arrays of Possible Character Choices
+// Created function to ask user which options and made conditional statements to ensure minimum option requirements were met.
+var numbers = window.confirm("Would you like to include numbers?");
+if (numbers === true) {
+  choose.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+}
 
+var upperCase = window.confirm("Would you like to include uppercase letters?");
+if (upperCase === true) {
+  choose.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+}
 
-  return "Generated password will go here!";
+var lowerCase = window.confirm("Would you like to include lowercase letters?");
+if (lowerCase === true) {
+  choose.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+}
+
+var special = window.confirm("Would you like to include special characters?");
+if (special === true) { 
+  choose.push('@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.');
+}
+
+  for (var i = 0; i < length; i++) {
+    secure += choose[Math.floor(Math.random() * choose.length)];
+  }
+
+  return secure;
 }
 
